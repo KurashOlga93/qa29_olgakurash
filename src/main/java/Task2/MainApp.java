d4d546bbaf9a3c7fcc2fa9cb7f92533886f1140b
o.kurash <olga9793@gmail.com>
2024-12-16 00:07:01 +0300

    lesson 6

---
+++ src/main/java/Task2/MainApp.java
@@ -0,0 +1,12 @@
+package Task2;
+
+import Task1.ProductReport;
+import Task1.Type;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        StaticMethod report = new StaticMethod();
+        report.myMethod("Hello!", 20);
+    }
+}
---
+++ src/main/java/Task2/StaticMethod.java
@@ -0,0 +1,12 @@
+package Task2;
+
+public class StaticMethod {
+
+
+    public String myMethod(String phrase, int count) {
+        for(int i = 0; i < count; i++)
+
+            System.out.println(phrase);
+        return phrase;
+    }
+}
