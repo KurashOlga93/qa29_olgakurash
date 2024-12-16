a5ac06169a9a53ee0dc40b471c7e068f9d2a55cf
o.kurash <olga9793@gmail.com>
2024-12-16 18:12:45 +0300

    lesson 6 (new)

--- src/main/java/Task2/MainApp.java
+++ src/main/java/Task2/MainApp.java
@@ -1,12 +1,8 @@
 package Task2;
 
-import Task1.ProductReport;
-import Task1.Type;
-
 public class MainApp {
     public static void main(String[] args) {
 
-        StaticMethod report = new StaticMethod();
-        report.myMethod("Hello!", 20);
+        StaticMethod.myMethod("Hello!", 20);
     }
 }
